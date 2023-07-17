16f73409bcbbacfd828e2b4c3a75c5d69abe6783
murilo <muril0.esteves@protonmail.com>
2023-07-17 12:26:33 -0400

    localizando parâmetro usando find() e len()

--- extrator_url/main.py
+++ extrator_url/main.py
@@ -1,14 +1,17 @@
-url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
-print(f'url completa: {url}')
+url = "https://bytebank.com/cambio?moedaOrigem=real"
 
 indice_interrogacao = url.find('?')
-
 url_base = url[:indice_interrogacao]
-print(f'base da url: {url_base}')
 
 url_parametros = url[indice_interrogacao+1:] #x+1 para excluir o separador
 print(f'parâmetros da url: {url_parametros}')
 
+parametro_de_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_de_busca)
+indice_valor = indice_parametro + len(parametro_de_busca) + 1
+valor = url_parametros[indice_valor:]
+
+print(f'valor: {valor}')
 #essa abordagem problemas pq a url podem mudar de posição
 
 #encontrar posição de operador na url usando fin()
